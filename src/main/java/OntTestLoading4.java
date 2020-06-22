5f76b18235b7e5c038e1b04eeb146da9e69bd89d
Chris Tomlinson <ct@moonvine.org>
2020-06-22 15:45:08 -0500

    updates for verifying ont-policy updates

--- src/main/java/OntTestLoading4.java
+++ src/main/java/OntTestLoading4.java
@@ -15,7 +15,11 @@ public class OntTestLoading4 {
 
     public static Logger logger = LoggerFactory.getLogger(OntTestLoading4.class);
 
-    private static final String ONT_POLICY = "https://raw.githubusercontent.com/buda-base/editor-templates/master/ont-policy.rdf";
+//    private static final String ONT_POLICY = "https://raw.githubusercontent.com/buda-base/editor-templates/master/ont-policy-local.rdf";
+    private static final String ONT_POLICY = "/Users/chris/git/editor-templates/ont-policy.rdf";
+//    private static final String ONT_POLICY = "/Users/chris/git/editor-templates/ont-policy-local.rdf";
+//    private static final String ONT_POLICY = "/Users/chris/git/owl-schema/ont-policy.rdf";
+//    private static final String ONT_POLICY = "/Users/chris/git/owl-schema/ont-policy-local.rdf";
     private static final String OUT = "/Users/chris/BUDA/TEST_OUTPUT/";
     private static final String PERSON_SHAPES = "http://purl.bdrc.io/shapes/core/PersonShapes/";
     private static final String PERSON_LOCAL_SHAPES = "http://purl.bdrc.io/shapes/core/PersonLocalShapes/";
@@ -39,7 +43,7 @@ public class OntTestLoading4 {
     }
     
     private static void doAll(String name) {
-        logger.info("processing ALL from {} imports {}", name);
+        logger.info("processing ALL from {}", name);
 
         odm.setProcessImports(true);
         odm.setCacheModels(true);
@@ -81,9 +85,9 @@ public class OntTestLoading4 {
     public static void main(String[] args){
         
         initOdm();
-        doAll("ALL_SHAPES07");
-        doTest(PERSON_LOCAL_SHAPES, true, "PersonLocalShapes_ALL07");
-        doTest(PERSON_SHAPES, true, "PersonShapes_ALL07");
+        doAll("ALL_ONT_09");
+//        doTest(PERSON_LOCAL_SHAPES, true, "PersonLocalShapes_ALL07");
+//        doTest(PERSON_SHAPES, true, "PersonShapes_ALL07");
 //        doTest(PERSON_UI_SHAPES, true, "PersonUIShapes_ALL07");
     }
 }
