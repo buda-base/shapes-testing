1c47a317f1597380c333b348212095fb7a874d89
eroux <elie.roux@telecom-bretagne.eu>
2020-06-24 12:45:52 +0200

    add test with resource manager

---
+++ src/main/java/OntTestLoading5_ONTS_RES.java
@@ -0,0 +1,69 @@
+import java.io.FileWriter;
+import java.util.Iterator;
+
+import org.apache.jena.ontology.OntDocumentManager;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OntTestLoading5_ONTS_RES {
+
+    public static Logger logger = LoggerFactory.getLogger(OntTestLoading5_SHAPES_LOCAL.class);
+
+    
+    private static final String OUT = "/tmp/";
+    
+    private static final String ONT_POLICY = "owl-schema/ont-policy.rdf";
+        
+    private static OntModelSpec oms;
+    private static OntDocumentManager odm;
+    
+    private static void initOdm() {
+        oms = new OntModelSpec(OntModelSpec.OWL_MEM);        
+        odm = new OntDocumentManager(ONT_POLICY);
+        oms.setDocumentManager(odm);
+        FileManager odFm = odm.getFileManager();
+        writeTtl(odFm.getLocationMapper().toModel(), "LOCATOR_SHAPES_LOCAL_05");
+    }    
+    
+    private static void writeTtl(Model m, String nm) {
+        try {
+            m.write(new FileWriter(OUT+nm+".ttl"), "TTL");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }        
+    }
+    
+    private static void doAll(String name) {
+        logger.info("processing ALL from {}", name);
+
+        odm.setProcessImports(true);
+        odm.setCacheModels(true);
+
+        Iterator<String> allItr = odm.listDocuments();
+        OntModel allModel = null;
+        while (allItr.hasNext()) {
+            String uri = allItr.next();
+            OntModel m = odm.getOntology(uri, oms);
+            logger.info("got model of size {} for uri {}", (m != null ? m.size() : 0), uri);
+            if (m != null) {
+                if (allModel == null)
+                    allModel = m;
+                else
+                    allModel.add(m);
+            }
+        }
+
+        System.out.println("\n\ndoAll("+name+").getModelSize() == "+allModel.size());
+        writeTtl(allModel, name);
+    }
+    
+    public static void main(String[] args){
+        
+        initOdm();
+        doAll("ALL_SHAPES_LOCAL_05");
+    }
+}
