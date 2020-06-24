a8b76c5989419895f1919e0adcc6cefe3f781b1b
Marc Agate <agate.marc@gmail.com>
2020-06-24 11:52:28 -0400

    workaround without ontDocManager, using classLoader and ont-Policy.rdf

---
+++ src/main/java/OntPolicy.java
@@ -0,0 +1,79 @@
+
+public class OntPolicy {
+
+    public String baseUri;
+    public String graph;
+    public String fileUri;
+    public String file;
+    public boolean visible;
+
+    public OntPolicy() {
+        super();
+        this.baseUri = "";
+        this.graph = "";
+        this.fileUri = "";
+        this.visible = false;
+    }
+
+    public OntPolicy(String baseUri, String graph, String fileuri, String file, boolean visible) {
+        super();
+        this.baseUri = parseBaseUri(baseUri);
+        this.graph = graph;
+        this.fileUri = fileuri;
+        this.file = file;
+        this.visible = visible;
+    }
+
+    private String parseBaseUri(String s) {
+        if (s.endsWith("/") || s.endsWith("#")) {
+            s = s.substring(0, s.length() - 1);
+        }
+        return s;
+    }
+
+    public String getBaseUri() {
+        return baseUri;
+    }
+
+    public void setBaseUri(String baseUri) {
+        this.baseUri = baseUri;
+    }
+
+    public String getGraph() {
+        return graph;
+    }
+
+    public void setGraph(String graph) {
+        this.graph = graph;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public String getFileUri() {
+        return fileUri;
+    }
+
+    public void setFileUri(String fileUri) {
+        this.fileUri = fileUri;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public String toString() {
+        return "OntPolicy [baseUri=" + baseUri + ", graph=" + graph + ", fileUri=" + fileUri + ", file=" + file + ", visible=" + visible + "]";
+    }
+
+}
--- src/main/java/OntTestLoading5_ONTS_RES.java
+++ src/main/java/OntTestLoading5_ONTS_RES.java
@@ -1,68 +1,120 @@
 import java.io.FileWriter;
-import java.util.Iterator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 import org.apache.jena.ontology.OntDocumentManager;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.RDF;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OntTestLoading5_ONTS_RES {
 
-    public static Logger logger = LoggerFactory.getLogger(OntTestLoading5_SHAPES_LOCAL.class);
+    public static Logger logger = LoggerFactory.getLogger(OntTestLoading5_ONTS_RES.class);
 
-    
     private static final String OUT = "/tmp/";
-    
+
     private static final String ONT_POLICY = "owl-schema/ont-policy.rdf";
-        
+
     private static OntModelSpec oms;
     private static OntDocumentManager odm;
-    
-    private static void initOdm() {
-        oms = new OntModelSpec(OntModelSpec.OWL_MEM);        
-        odm = new OntDocumentManager(ONT_POLICY);
-        oms.setDocumentManager(odm);
-        FileManager odFm = odm.getFileManager();
-        writeTtl(odFm.getLocationMapper().toModel(), "LOCATOR_ONT_RES_05");
-    }    
-    
+    private static Model policies;
+    static HashMap<String, OntPolicy> map;
+
+    private static void initOdm() throws IOException {
+        map = new HashMap<>();
+        policies = ModelFactory.createDefaultModel();
+        InputStream in = OntTestLoading5_ONTS_RES.class.getClassLoader().getResourceAsStream(ONT_POLICY);
+        policies.read(in, RDFLanguages.strLangRDFXML);
+        ResIterator it1 = policies.listResourcesWithProperty(RDF.type,
+                ResourceFactory.createResource("http://jena.hpl.hp.com/schemas/2003/03/ont-manager#OntologySpec"));
+        while (it1.hasNext()) {
+            Resource r = it1.next();
+            OntPolicy op = loadPolicy(r, FileManager.get().clone(), "core");
+            map.put(op.getBaseUri() + "/", op);
+            logger.info("loaded OntPolicy for uri {} >> {} ", op.getBaseUri(), op);
+        }
+        in.close();
+    }
+
     private static void writeTtl(Model m, String nm) {
         try {
-            m.write(new FileWriter(OUT+nm+".ttl"), "TTL");
+            m.write(new FileWriter(OUT + nm + ".ttl"), "TTL");
         } catch (Exception ex) {
             ex.printStackTrace();
-        }        
+        }
     }
-    
-    private static void doAll(String name) {
+
+    private static void doAll(String name) throws IOException {
         logger.info("processing ALL from {}", name);
-
-        odm.setProcessImports(true);
-        odm.setCacheModels(true);
-
-        Iterator<String> allItr = odm.listDocuments();
-        OntModel allModel = null;
-        while (allItr.hasNext()) {
-            String uri = allItr.next();
-            OntModel m = odm.getOntology(uri, oms);
-            logger.info("got model of size {} for uri {}", (m != null ? m.size() : 0), uri);
-            if (m != null) {
-                if (allModel == null)
-                    allModel = m;
-                else
-                    allModel.add(m);
+        Model allModel = null;
+        InputStream in = null;
+        for (String uri : map.keySet()) {
+            // String uri = allItr.next();
+            logger.info("URI {}", uri);
+            if (map.keySet().contains(uri)) {
+                Model m = ModelFactory.createDefaultModel();
+                in = OntTestLoading5_ONTS_RES.class.getClassLoader().getResourceAsStream(getJarLoc(uri));
+                System.out.println("jarLOC >>" + getJarLoc(uri) + " in:" + in);
+                m.read(in, "", "TTL");
+                logger.info("got model of size {} for uri {}", (m != null ? m.size() : 0), uri);
+                if (m != null) {
+                    if (allModel == null)
+                        allModel = m;
+                    else
+                        allModel.add(m);
+                }
             }
         }
-
-        System.out.println("\n\ndoAll("+name+").getModelSize() == "+allModel.size());
+        in.close();
+        System.out.println("\n\ndoAll(" + name + ").getModelSize() == " + allModel.size());
+        OntModel ontm = ModelFactory.createOntologyModel();
+        ontm.addSubModel(allModel);
+        System.out.println("\n\ndoAll(" + name + ").getModelSize() == " + ontm.size());
         writeTtl(allModel, name);
     }
-    
-    public static void main(String[] args){
-        
+
+    private static OntPolicy loadPolicy(Resource r, FileManager fm, String type) {
+        Statement st = r.getProperty(ResourceFactory.createProperty("http://jena.hpl.hp.com/schemas/2003/03/ont-manager#publicURI"));
+        String baseUri = st.getObject().asResource().getURI();
+        String graph = null;
+        st = r.getProperty(ResourceFactory.createProperty("http://purl.bdrc.io/ontology/admin/ontGraph"));
+        if (st != null) {
+            graph = st.getObject().asResource().getURI();
+        }
+        boolean visible = false;
+        st = r.getProperty(ResourceFactory.createProperty("http://purl.bdrc.io/ontology/admin/ontVisible"));
+        if (st != null) {
+            visible = st.getObject().asLiteral().getBoolean();
+        }
+        st = r.getProperty(ResourceFactory.createProperty("http://jena.hpl.hp.com/schemas/2003/03/ont-manager#altURL"));
+        String file = "";
+        if (st != null) {
+            file = st.getObject().toString();
+        }
+        OntPolicy op = new OntPolicy(baseUri, graph, fm.mapURI(baseUri), file, visible);
+        return op;
+    }
+
+    public static String getJarLoc(String uri) {
+        OntPolicy op = map.get(uri);
+        logger.info("Getting jarLoc for {} and policy {}", uri, op);
+        String tmp = op.getFile();
+        tmp = "owl-schema/" + tmp.substring(tmp.lastIndexOf("RDF/") + 4);
+        return tmp;
+    }
+
+    public static void main(String[] args) throws IOException {
         initOdm();
         doAll("ALL_ONTS_RES_05");
     }
