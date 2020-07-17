7da665d8ac366e960054338b1f0f48bd3922f57e
Chris Tomlinson <ct@moonvine.org>
2020-07-17 12:01:08 -0500

    update to jena 3.16.0; add simple parse error test

---
+++ src/main/java/DualFullValidateTest03.java
@@ -0,0 +1,35 @@
+import org.apache.jena.graph.Graph;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shacl.ShaclValidator;
+import org.apache.jena.shacl.Shapes;
+import org.apache.jena.shacl.ValidationReport;
+
+public class DualFullValidateTest03 {
+
+    static final String DATA = "P707_CL03.ttl";
+
+    static Graph testGraph;
+    static Model testModel;
+
+    static {
+        testGraph = RDFDataMgr.loadGraph(DATA);
+        testModel = ModelFactory.createModelForGraph(testGraph);
+    }
+
+    public static void main(String... args) {
+        try {
+            ShaclValidator sv = ShaclValidator.get();
+            Shapes shapes = Shapes.parse(testGraph);
+            ValidationReport report = sv.validate(shapes, testGraph);
+            if (report != null) {
+                report.getModel().write(System.out, "TURTLE");
+            } else {
+                System.out.println("Jena validation is null !");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
